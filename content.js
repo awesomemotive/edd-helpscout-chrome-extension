3c0fcadcb46fc65759b9c55702ec5ddc26e2ea60
Phil Johnston <support@mintplugins.com>
2016-04-18 14:09:39 -0400

    Simple Shipping

--- content.js
+++ content.js
@@ -15,7 +15,8 @@ function eddStrings() {
         "edd donations",
         "fraud monitor",
         "edd stripe",
-        "paypal adaptive payments"
+        "paypal adaptive payments",
+		"simple shipping"
     ];
     return strings;
 }
@@ -36,7 +37,8 @@ function eddURLs() {
         "https://easydigitaldownloads.com/downloads/donations/",
         "https://easydigitaldownloads.com/downloads/fraud-monitor/",
         "https://easydigitaldownloads.com/downloads/stripe/",
-        "https://easydigitaldownloads.com/downloads/paypal-adaptive-payments/"
+        "https://easydigitaldownloads.com/downloads/paypal-adaptive-payments/",
+		"https://easydigitaldownloads.com/downloads/simple-shipping/"
     ];
     return urls;
 }
